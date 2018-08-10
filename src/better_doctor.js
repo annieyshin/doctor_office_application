e333a1aad9590932e3f264bcf0494aeb27bbe300
Annie Shin <annieyshin@gmail.com>
2018-08-10 15:32:24 -0700

    figuring out params

--- src/better_doctor.js
+++ src/better_doctor.js
@@ -3,12 +3,12 @@ const apiKey = require('./../.env').apiKey;
 export class DoctorAPI {
   getDoctors(attr) {
     let name = attr.name;
-    let location = attr.location;
+    // let location = attr.location;
     let query = attr.query;
 
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=or-portland&skip=0&limit=20&user_key=${apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=or-portland&user_key=${apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
