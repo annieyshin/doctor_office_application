541c9d79b8bf6813a14c246f69c4f7abd7dde6aa
Annie Shin <annieyshin@gmail.com>
2018-08-12 14:09:18 -0700

    add ability to display default value for website or show url if available

--- src/main.js
+++ src/main.js
@@ -15,14 +15,23 @@ $(document).ready(function() {
       let body = JSON.parse(response);
       console.log(body.data);
       body.data.forEach(function(doctor) {
-        $('#results').append(
-        `<div class="card">
-          <h1>${doctor.profile.first_name} ${doctor.profile.last_name} </h1>
-          <h2>${doctor.practices[0].website} </h2>
-          <h2>${doctor.practices[0].phones} </h2>
-          <h3><em>Address:</em> ${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state}, ${doctor.practices[0].visit_address.zip} </h3>
-        </div>`
-        );
+      let website = doctor.practices[0].website;
+        for (let i = 0; i <= 0; i++) {
+          if (website !== undefined) {
+            website = doctor.practices[0].website;
+          }
+          else {
+            website = "not available"
+          }
+            $('#results').append(
+            `<div class="card">
+              <h1> Name: ${doctor.profile.first_name} ${doctor.profile.last_name} </h1>
+              <h3>Contact Info: ${doctor.practices[0].phones[i].number} </h3>
+              <h3> Website: ${website}</h3>
+              <h4>Address: ${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state}, ${doctor.practices[0].visit_address.zip} </h4>
+            </div>`
+            );
+          }
       });
     });
   });
