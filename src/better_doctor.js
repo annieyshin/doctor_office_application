4310e01d8f6b303ffe71dd56623da7e300725aa3
Annie Shin <annieyshin@gmail.com>
2018-08-29 17:19:49 -0700

    add in process.env.export.apiKey

--- src/better_doctor.js
+++ src/better_doctor.js
@@ -6,7 +6,7 @@ export class DoctorAPI {
 
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=or-portland&limit=10&user_key=${apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=or-portland&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
